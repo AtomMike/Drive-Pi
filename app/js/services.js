7f6c7f11d71c6ddb332ff197efabe45f7e05de60
Mike Bailey <michaelbailey81@googlemail.com>
2016-06-26 22:51:25 +0100

    added getdirectory factory

--- app/js/services.js
+++ app/js/services.js
@@ -1,52 +1,105 @@
 angular.module('DrivePi.music', [])
 
-/**
- * Service providing playback and mpd queue functionality
- */
-.factory('mpdServices', function($http, $log, $rootScope, $q) {
+.factory('MPDServices', function($q, $timeout, $http, $log, $rootScope, $q) {
 
+	// Get the currently playing song's details:
 	function getPlaying() {
 		var currentSong = mpdClient.getCurrentSong();
 
 		if (currentSong) {
-			var songArtist = currentSong.getArtist();
-			// var albumArt = findAlbumArt(songArtist, 'feature');
+			var artistInfo = currentSong.getArtist();
 			var trackMetadata = currentSong.getMetadata()
-			var songObj = {
-	            'id' :  currentSong.getId(),
-	            'name' :  currentSong.getDisplayName(),
-	            'playTime' : {
-	            				'raw': mpdClient.getCurrentSongTime()
-	            			},
-	            'duration' : {
-	            				'raw': currentSong.getDuration()
-	            			},
-	            'artist' : songArtist,
-	            'album' : currentSong.getAlbum(),
-	            'year' : trackMetadata.date,
-				'next' : false,
-				'paused' : 0
-	        }
-
-	        var nextSong = mpdClient.getNextSong();
-
-	        if (nextSong) {
-
-	        	// var albumArt = findAlbumArt(nextSong.getArtist());
-				    //     var nextSongObj = {
-		        //     'id' :  nextSong.getId(),
-		        //     'name' :  nextSong.getDisplayName(),
-		        //     'duration' : nextSong.getDuration(),
-		        //     'artist' : nextSong.getArtist(),
-		        //     'album' : nextSong.getAlbum(),
-		        //     'queueid' : nextSong.getQueuePosition(),
-		        //     'image' : albumArt
-		        // }
-
-		        // songObj.next = nextSongObj;
-	        }
-	        return songObj;
+			var songObject = {
+        'id' :  currentSong.getId(),
+        'name' :  currentSong.getDisplayName(),
+        'playTime' : mpdClient.getCurrentSongTime(),
+        'duration' : currentSong.getDuration(),
+        'artist' : artistInfo,
+        'album' : currentSong.getAlbum(),
+        'year' : trackMetadata.date
+      }
+      return songObject;
 		}
+
+	}
+
+	var getDirectory = function(dirPath) {
+		var directoryContents = [];
+		var deferred_directoryContents = $q.defer();
+
+		  // Get directories:
+		  mpdClient.getDirectoryContents(dirPath, function(directoryFiles){
+				// console.log(dirPath);
+		    directoryFiles.forEach(function(directoryItem){
+		      var metaData 	= directoryItem.getMetadata();
+		      var path     	= directoryItem.getPath();
+					var trackId		= metaData.track;
+
+					if (typeof metaData.track !== 'undefined') {
+						var track = directoryItem.getTrack();
+						// console.log(track);
+						var queueSong = directoryItem.getQueueSong();
+						if (queueSong) {
+							trackId = queueSong.getId();
+						}
+						// console.log(trackId);
+					}
+					// console.log(directoryItem);
+
+					// if (typeof path !== 'undefined') {
+						// var dirName = path.replace(/\//g, '/');
+						var pathArray = path.split('/');
+						var dirName = pathArray[pathArray.length-1];
+						var itemData = {
+							'dirname': dirName,
+							'path': path,
+							'pathArray': pathArray,
+							'modified': metaData.last_modified,
+							'trackId': trackId,
+							'track': metaData.title,
+							'album': metaData.album,
+							'link': encodeURIComponent(path.replace("/", "@!@"))
+						}
+			      directoryContents.push(itemData);
+					// }
+		    });
+				// console.log(directoryContents);
+		  });
+
+			$timeout(function() {
+				deferred_directoryContents.resolve(directoryContents);
+			}, 750);
+
+			return deferred_directoryContents.promise;
+
+	}
+
+	var currentQueue = function(){
+		var mpdQueue		 = mpdClient.getQueue();
+		var currentSong	 = mpdClient.getCurrentSongID();
+
+  	mpdQueue.getSongs().forEach(function(playlistSong){
+			var songObject = {
+				'id' :  playlistSong.getId(),
+				'name' :  playlistSong.getDisplayName(),
+				'duration' : playlistSong.getDuration(),
+				'artist' : playlistSong.getArtist(),
+				'album' : playlistSong.getAlbum(),
+				'year' : playlistSong.date
+			}
+			// console.log(songObject);
+		})
+
+		return currentSong;
+	}
+
+	var getMsg = function(){
+		var deferred = $q.defer();
+    $timeout(function() {
+      deferred.resolve(['Hello', 'world!']);
+    }, 2000);
+
+    return deferred.promise;
 
 	}
 
@@ -55,6 +108,16 @@ angular.module('DrivePi.music', [])
     getPlaying: function() {
     	return getPlaying();
     },
+
+    currentQueue: function() {
+    	return currentQueue();
+    },
+
+    getDirectory: function(dirPath) {
+    	return getDirectory(dirPath);
+    },
+
+		getMsg:getMsg
 
 	}
 
